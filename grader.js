01e895c74cd00f22fc67d521358221a844ae7dfb
Gal Bracha <galbra@gmail.com>
2013-07-10 10:45:07 +0000

    grader now support loading from url

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -40,13 +41,23 @@ var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+var downloadUrl = function(url, callback) {
+	rest.get(url).on('complete', function(result) {
+  		if (result instanceof Error) {
+    			sys.puts('Error: ' + result.message);
+    			this.retry(5000); // try again after 5 sec
+	  	} else {
+			callback(null, result);
+ 		}
+	});
+}
+
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
+var checkHtml = function(html, checks) {
+    $ = cheerio.load(html);
     var out = {};
     for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
@@ -61,14 +72,29 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var mainGrader = function(error, html) {
+	if(error) {
+		sys.puts('Error fetching html:' + error);
+		process.exit(1);
+	}
+	var checks = loadChecks(program.checks).sort();
+	var checkJson = checkHtml(html, checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+}
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url  <url>', 'Path to url')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	
+	if(program.url) {
+		downloadUrl(program.url, mainGrader);
+	}else if (program.file) {
+		fs.readFile(program.file, mainGrader);
+	}
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
